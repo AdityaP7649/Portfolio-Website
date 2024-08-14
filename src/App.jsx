ce18ce68d53a19a69eff222d81e072c0b02ebb77
AdityaP7649 <adityapatil7649@gmail.com>
2024-08-14 23:42:15 +0530

    Added Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
-import './App.css'
+import Navbar from "./components/navbar/Navbar"
+import "./App.css"
 
 function App() {
 
   return (
-    <>
-    <h1>Hello</h1>
-    </>
+    <div className="main-container"> 
+      <Navbar/>
+    </div>
   )
 }
 
