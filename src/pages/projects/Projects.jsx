0c2829701f6f3cdbf025ef57913e2d1ccad0af94
AdityaP7649 <adityapatil7649@gmail.com>
2024-09-01 23:50:15 +0530

    Added links to projects

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -16,31 +16,44 @@ function Projects() {
       </div>
 
       <div className="projects-main-container">
-        <Card
-          projectTitle="Blog Site"
-          projectDescription="Blog Site  is a platform for sharing and exploring stories, inspired by Medium. Users can create, update, and delete blogs, engaging with community sharing insights and experiences."
-          projectImage={Blog}
-        />
-        <Card
-          projectTitle="Rick and Morty Toy Store"
-          projectDescription="Developed a fully functional vacation rental website using the MERN stack, featuring property listings, booking management, and user authentication."
-          projectImage={RickMorty}
-        />
-        <Card
-          projectTitle="SweetSpot"
-          projectDescription="Developed a fully functional vacation rental website using the MERN stack, featuring property listings, booking management, and user authentication."
-          projectImage={Sweet}
-        />
-        <Card
-          projectTitle="Vacation Rental Platform"
-          projectDescription="Developed a fully functional vacation rental website using the MERN stack, featuring property listings, booking management, and user authentication."
-          projectImage={Vacation}
-        />
-        <Card
-          projectTitle="Spotify Clone"
-          projectDescription="Created a Spotify clone utilizing modern web development tools, featuring a responsive design, music streaming functionality, and a user-friendly interface."
-          projectImage={Spotify}
-        />
+        <a href="https://friday-blog.netlify.app/">
+          <Card
+            projectTitle="Blog Site"
+            projectDescription="Blog Site  is a platform for sharing and exploring stories, inspired by Medium. Users can create, update, and delete blogs, engaging with community sharing insights and experiences."
+            projectImage={Blog}
+          />
+        </a>
+        <a href="https://side-projects-overkill.github.io/rick-and-morty-toy-store/">
+          <Card
+            projectTitle="Rick and Morty Toy Store"
+            projectDescription="Developed a fully functional vacation rental website using the MERN stack, featuring property listings, booking management, and user authentication."
+            projectImage={RickMorty}
+          />
+        </a>
+
+        <a href="https://app.netlify.com/sites/sweetspot-shop/overview">
+          <Card
+            projectTitle="SweetSpot"
+            projectDescription="Developed a fully functional vacation rental website using the MERN stack, featuring property listings, booking management, and user authentication."
+            projectImage={Sweet}
+          />
+        </a>
+
+        <a href="https://vacation-rental-platform.onrender.com/listings">
+          <Card
+            projectTitle="Vacation Rental Platform"
+            projectDescription="Developed a fully functional vacation rental website using the MERN stack, featuring property listings, booking management, and user authentication."
+            projectImage={Vacation}
+          />
+        </a>
+
+        <a href="https://spotify-clone-proj1.netlify.app/">
+          <Card
+            projectTitle="Spotify Clone"
+            projectDescription="Created a Spotify clone utilizing modern web development tools, featuring a responsive design, music streaming functionality, and a user-friendly interface."
+            projectImage={Spotify}
+          />
+        </a>
         <Card
           projectTitle="Real Time Campus Navigation System"
           projectDescription="Developed a real-time Campus Navigation App to help students, staff, and visitors easily navigate and explore the campus with step-by-step directions and live location tracking."
